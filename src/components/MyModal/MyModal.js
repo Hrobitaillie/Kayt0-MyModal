ef3f7a570e66fe1e34464bbe205b2f517089abc7
Hugo Robitaillie <hugo@la-ligne-web.com>
2023-02-16 10:38:20 +0100

    commentary added

--- src/components/MyModal/MyModal.js
+++ src/components/MyModal/MyModal.js
@@ -3,7 +3,7 @@ import "../../styles/MyModal";
 import React from "react";
 import { CloseIcon } from "../CloseIcon";
 
-export const MyModal = ({children, trigger, onClose }) => {
+export const MyModal = ({ children, trigger, onClose }) => {
   const displayAnimations = {
     open: {
       opacity: 1,
@@ -16,11 +16,24 @@ export const MyModal = ({children, trigger, onClose }) => {
   };
 
   return (
-    <section className={"w-full h-full absolute top-1--2 left-1--2 -translate-x-1--2 -translate-y-1--2 "+  (trigger ? "pointer-events-all" : "pointer-events-none") }>
+    <section
+      className={
+        "w-full h-full absolute top-1--2 left-1--2 -translate-x-1--2 -translate-y-1--2 " +
+        (trigger ? "pointer-events-all" : "pointer-events-none")
+      }
+    >
       <div
         className={
-          " w-screen h-screen absolute top-1--2 left-1--2 -translate-x-1--2 -translate-y-1--2 bg-white opacity-70 .pointer-events-all "+  (trigger ? "block" : "hidden")}
+          " w-screen h-screen absolute top-1--2 left-1--2 -translate-x-1--2 -translate-y-1--2 bg-white opacity-70 .pointer-events-all " +
+          (trigger ? "block" : "hidden")
+        }
       ></div>
+      {/* 
+        Usage of motion to animate de div, and pass props to configure the animation 
+        Animate is the condition to start the animation, use variants name to devine wich animation you want
+        Variant is an object of differents animations
+        initial is the initial animation.
+      */}
       <motion.div
         className={
           "p-1r absolute top-1--2 left-1--2 -translate-x-1--2 -translate-y-1--2 min-w-400 shadow-popup rounded overflow-hidden bg-white .pointer-events-all"
@@ -30,11 +43,14 @@ export const MyModal = ({children, trigger, onClose }) => {
         initial="close"
       >
         <span
-            className=" cursor-pointer ml-2 hover-scale-150 transition-all absolute top-1r right-1r"
-            onClick={onClose}
-          >
-            <CloseIcon />
-          </span>
+          className=" cursor-pointer ml-2 hover-scale-150 transition-all absolute top-1r right-1r"
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </span>
+
+        {/* pass the children props to display user elements */}
+
         {children}
       </motion.div>
     </section>
